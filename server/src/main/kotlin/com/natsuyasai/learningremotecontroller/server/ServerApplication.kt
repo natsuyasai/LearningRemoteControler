8697ffb9856b3f05d00afe1a0f594df86fe01faf
natsuyasai <yasai038tomato@gmail.com>
2024-01-08 16:40:44 +0900

    graphqlベースライン作成

--- server/src/main/kotlin/com/natsuyasai/learningremotecontroller/server/ServerApplication.kt
+++ server/src/main/kotlin/com/natsuyasai/learningremotecontroller/server/ServerApplication.kt
@@ -5,7 +5,8 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan("com.natsuyasai.learningremotecontroller.repository")
+@ComponentScan("com.natsuyasai.learningremotecontroller.server.repository")
+@ComponentScan("com.natsuyasai.learningremotecontroller.server.controller")
 class ServerApplication
 
 fun main(args: Array<String>) {
